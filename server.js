e185cf638d653cb6cf13b62eb1c68b200d167842
Briana M Watson <bmwatson@Brianas-MacBook-Pro.local>
2019-02-18 17:20:49 -0700

    Adding images

---
+++ server.js
@@ -0,0 +1,101 @@
+const express = require("express");
+var exphbs = require('express-handlebars');
+
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cheerio = require("cheerio");
+const logger = require("morgan");
+const request = require("request");
+const axios = require("axios");
+//require models
+const db = require("./models");
+const PORT = process.env.PORT || 3000;
+
+
+var app = express();
+app.engine('handlebars', exphbs({ defaultLayout: 'index' }));
+app.set('view engine', 'handlebars');
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+app.get("/scrape", function (req, res) {
+  axios.get("https://news.yahoo.com/").then(function (response) {
+
+    var $ = cheerio.load(response.data);
+
+    $("a.lead").each(function (i, element) {
+      var result = {};
+
+      result.title = $(this)
+        .text();
+
+      result.link = $(this)
+        .attr("href");
+
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          return res.json(err);
+        });
+    });
+
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/", function (req, res) {
+  res.render('main');
+})
+
+app.get("/articles", function (req, res) {
+  db.Article.find({})
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+      console.log(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function (req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function (req, res) {
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function (dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
